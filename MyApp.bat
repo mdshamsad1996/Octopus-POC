64a7a95a8e78c36baacee41e8cc325af9cc73480
unknown <alam.shams.1996@gmail.com>
2019-11-14 19:44:28 +0530

    MyApp.bat for creating nuget package

---
+++ MyApp.bat
@@ -0,0 +1,2 @@
+del *.nupkg
+nuget.exe pack MyApp.nuspec -Prop Configuration=Debug -Version 1.0.0
--- MyTest.bat
+++
@@ -1,2 +0,0 @@
-del *.nupkg
-nuget.exe pack WebApplication3.nuspec -Prop Configuration=Debug -Version 1.0.0
