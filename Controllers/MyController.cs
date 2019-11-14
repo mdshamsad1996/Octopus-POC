e9c76ec916a4437c9c4eadd0f5f90938a46ae38c
unknown <alam.shams.1996@gmail.com>
2019-11-14 19:37:52 +0530

    Octopus-POC

---
+++ Controllers/MyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class MyController : ApiController
+    {
+        [HttpGet]
+        [Route ("api/HelloWorld")]
+        public string Get()
+        {
+            string env = ConfigurationSettings.AppSettings.Get("Env");
+            return "Hello World from "+env;
+        }
+
+    }
+}
---
+++ MyTest.bat
@@ -0,0 +1,2 @@
+del *.nupkg
+nuget.exe pack WebApplication3.nuspec -Prop Configuration=Debug -Version 1.0.0
